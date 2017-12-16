d92359fe21376b6a8a3a433761a39e5983952633
JamesTown-625 <{JamesTown-625}@users.noreply.github.com>
2017-12-15 20:59:05 -0700

    added screenshots of the command line and improve the look a little bit.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Tatonka_767",
+  password: "",
   database: "bamazon_db"
 });
 
@@ -30,12 +30,17 @@ function start() {
           type: "list",
           choices: function() {
             var choiceArray = [];
+            console.log("\n");
+            console.log("================ Welcome to BAMAZON ====================");
+            console.log("Take a look below for the items we have for sale:")
             for (var i = 0; i < results.length; i++) {
             console.log(results[i].item_id, results[i].product_name, results[i].price);
               choiceArray.push(results[i].product_name);
             }
+            console.log("========================================================");
             return choiceArray;
           },
+
           message: "What are you interested in buying?"
         },
         {
@@ -79,7 +84,7 @@ function start() {
               console.log("\n");
               console.log("Item successfully purchased!");
               console.log("\n");
-              console.log("You're grand total is: " + grandTotal);
+              console.log("Your grand total is: " + grandTotal);
               console.log("\n");
               console.log("=============================================");
               keepShopping();
@@ -90,7 +95,7 @@ function start() {
           // stock quantity wasn't high enough, so apologize and start over
           console.log("=============================================");
           console.log("\n");
-          console.log("We apologize... We only have:" + chosenItem.stock_quantity + " " + chosenItem.product_name + "(s) left");
+          console.log("We apologize... We don't have a sufficient amount of " + chosenItem.product_name + "(s) to complete your order. Try again.");
           keepShopping();
         }
       });
@@ -106,7 +111,7 @@ function keepShopping() {
 			message: "Would you like to keep shopping?"
 		}
 	]).then(function(answer) {
-		console.log(answer.startOver)
+		// console.log(answer.startOver)
 		if (answer.startOver) {
 
 			start();
